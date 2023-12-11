ebefa4db19202a3049d6156a0f5c2880ba9195e5
da-takatsuki <da-takatsuki@kddi.com>
2023-12-11 13:27:37 +0900

    hello world

---
+++ README.md
@@ -0,0 +1,15 @@
+## 作るもの
+
+Hello World
+
+## 参考
+
+https://qiita.com/Sicut_study/items/3c5cd798313854a471a0
+
+## 技術スタック
+
+- pnpm
+- TypeScript
+- vue
+- vite
+- vercel
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="body">
+    <div id="text">Hello World</div>
+  </div>
+</template>
+
+<style scoped>
+.body {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+#text {
+  font-size: 50px;
+  font-weight: bold;
+  font-family: fantasy;
+  color: cyan;
+}
+</style>
